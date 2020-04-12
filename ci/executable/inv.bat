b4a2a1160a397198847c6dd0ca47bdfdc75c4208
Benj Boyer <benj_boyer@hotmail.com>
2020-04-12 13:05:40 -0400

    * Fixed executables (+ commenting)

--- ci/executable/inv.bat
+++ ci/executable/inv.bat
@@ -1,3 +1,13 @@
-set batdir=%~dp0
+@ECHO off
 
-java -jar %batdir%../lib/${project.artifactId}-${project.version}.jar "%*"
+REM Get parent directory of this script
+SET BASEDIR=%~dp0
+
+REM Get lib directory
+SET LIBDIR=%BASEDIR%../lib
+
+REM Get INV jar
+SET INVJAR=${project.artifactId}-${project.version}.jar
+
+REM Call INV jar with parameters
+java -jar %LIBDIR%/%INVJAR% "%*"

33343b70baf5418df47a328bbf3400bf5608a9e4
Benj Boyer <15024379+benjboyer@users.noreply.github.com>
2020-05-26 09:21:19 -0400

    * Updated the init file example with the latest changes.

--- examples/init/init.groovy
+++ examples/init/init.groovy
@@ -1,15 +1,30 @@
 scm {
-    name "main" // Atleast one scm named 'main' is required.
+    name "main" // The name 'main' is mandatory.
 
     path "${env.TEMP ?: '/tmp'}/inv/init" // Change for your need
     src "https://github.com/peasoupio/inv.git"
 
     hooks {
+
+        // Do the actual source code extraction (or clone in "git" terms).
         init """
-# Remove '#' to use example composer workspace 
-# git clone -b master ${src} ./gitExtract
-# mv ./gitExtract/examples/composer/scms/* ./scms
-# rm -rf ./gitExtract
+git clone -b release/0.6-beta ${src} .
+
+mkdir ./scms
+cp examples/composer/scms/* ./scms
+"""
+        // Pull the latest changes.
+        pull """
+git reset --hard
+git pull
+
+cp examples/composer/scms/* ./scms
+"""
+        // Returns the current local repository branch as a version.
+        version """
+git rev-parse --abbrev-ref HEAD
 """
     }
 }
+
+

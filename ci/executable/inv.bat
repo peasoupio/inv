c224fa8a4cad2195a524a64d16d5ab4f812074e5
Benj Boyer <15024379+benjboyer@users.noreply.github.com>
2020-06-09 17:14:47 -0400

    * Removed Classloading changes and added java props for the system class loader at the executables level

--- ci/executable/inv.bat
+++ ci/executable/inv.bat
@@ -9,5 +9,8 @@ SET LIBDIR=%BASEDIR%../lib
 REM Get INV jar
 SET INVJAR=${project.artifactId}-${project.version}.jar
 
+REM Set Java properties
+SET JAVAPROPS=-Djava.system.class.loader=groovy.lang.GroovyClassLoader
+
 REM Call INV jar with parameters
-java -jar %LIBDIR%/%INVJAR% %*
+java %JAVAPROPS% -jar %LIBDIR%/%INVJAR% %*
--- src/main/java/io/peasoup/inv/security/CommonLoader.java
+++ src/main/java/io/peasoup/inv/security/CommonLoader.java
@@ -53,8 +53,8 @@ public class CommonLoader {
         compilerConfiguration.addCompilationCustomizers(secureASTCustomizer);
 
         this.secureMode = secureMode;
-        this.generalClassLoader = new GroovyClassLoader(ClassLoader.getSystemClassLoader());
-        this.securedClassLoader = new GroovyClassLoader(ClassLoader.getSystemClassLoader(), compilerConfiguration);
+        this.generalClassLoader = new GroovyClassLoader();
+        this.securedClassLoader = new GroovyClassLoader(Thread.currentThread().getContextClassLoader(), compilerConfiguration);
     }
 
     public Script parseClass(File file) throws IOException, IllegalAccessException, InstantiationException, NoSuchMethodException, InvocationTargetException {
